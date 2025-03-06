b4b1f07c48635b54ed180b84cdb2df0e3342c74b
rmoon187 <rabiul187@outlook.com>
2025-03-06 10:12:42 +0600

    fixed and deployed

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server
-        await client.connect();
+        // await client.connect();
 
         const database = client.db("sportsDb");
         const productsCollection = database.collection("sportsProducts");
